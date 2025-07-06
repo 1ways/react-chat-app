e99005997d83590a6142751394b991a0e3ad3d29
1ways <grutoha@gmail.com>
2025-07-06 13:42:05 +0200

    design chat sidebar

---
+++ src/components/ChatSidebar.jsx
@@ -0,0 +1,94 @@
+import SearchBar from './SearchBar';
+
+export default function ChatSidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__search">
+                <SearchBar />
+            </div>
+            <ul className="sidebar__chats">
+                <li className="sidebar__chats-item" tabIndex={0}>
+                    <div className="sidebar__chats-avatar"></div>
+                    <div className="sidebar__chats-content">
+                        <div className="sidebar__content-top">
+                            <p className="sidebar__content-title">John Doe</p>
+                            <p className="sidebar__content-time">9:55 AM</p>
+                        </div>
+                        <p className="sidebar__chats-last">Hey how was your day?</p>
+                    </div>
+                </li>
+                <li className="sidebar__chats-item" tabIndex={0}>
+                    <div className="sidebar__chats-avatar"></div>
+                    <div className="sidebar__chats-content">
+                        <div className="sidebar__content-top">
+                            <p className="sidebar__content-title">John Doe</p>
+                            <p className="sidebar__content-time">9:55 AM</p>
+                        </div>
+                        <p className="sidebar__chats-last">Hey how was your day?</p>
+                    </div>
+                </li>
+                <li className="sidebar__chats-item" tabIndex={0}>
+                    <div className="sidebar__chats-avatar"></div>
+                    <div className="sidebar__chats-content">
+                        <div className="sidebar__content-top">
+                            <p className="sidebar__content-title">John Doe</p>
+                            <p className="sidebar__content-time">9:55 AM</p>
+                        </div>
+                        <p className="sidebar__chats-last">Hey how was your day?</p>
+                    </div>
+                </li>
+                <li className="sidebar__chats-item" tabIndex={0}>
+                    <div className="sidebar__chats-avatar"></div>
+                    <div className="sidebar__chats-content">
+                        <div className="sidebar__content-top">
+                            <p className="sidebar__content-title">John Doe</p>
+                            <p className="sidebar__content-time">9:55 AM</p>
+                        </div>
+                        <p className="sidebar__chats-last">Hey how was your day?</p>
+                    </div>
+                </li>
+                <li className="sidebar__chats-item" tabIndex={0}>
+                    <div className="sidebar__chats-avatar"></div>
+                    <div className="sidebar__chats-content">
+                        <div className="sidebar__content-top">
+                            <p className="sidebar__content-title">John Doe</p>
+                            <p className="sidebar__content-time">9:55 AM</p>
+                        </div>
+                        <p className="sidebar__chats-last">Hey how was your day?</p>
+                    </div>
+                </li>
+                <li className="sidebar__chats-item" tabIndex={0}>
+                    <div className="sidebar__chats-avatar"></div>
+                    <div className="sidebar__chats-content">
+                        <div className="sidebar__content-top">
+                            <p className="sidebar__content-title">John Doe</p>
+                            <p className="sidebar__content-time">9:55 AM</p>
+                        </div>
+                        <p className="sidebar__chats-last">Hey how was your day?</p>
+                    </div>
+                </li>
+                <li className="sidebar__chats-item" tabIndex={0}>
+                    <div className="sidebar__chats-avatar"></div>
+                    <div className="sidebar__chats-content">
+                        <div className="sidebar__content-top">
+                            <p className="sidebar__content-title">John Doe</p>
+                            <p className="sidebar__content-time">9:55 AM</p>
+                        </div>
+                        <p className="sidebar__chats-last">Hey how was your day?</p>
+                    </div>
+                </li>
+                <li className="sidebar__chats-item" tabIndex={0}>
+                    <div className="sidebar__chats-avatar"></div>
+                    <div className="sidebar__chats-content">
+                        <div className="sidebar__content-top">
+                            <p className="sidebar__content-title">John Doe</p>
+                            <p className="sidebar__content-time">9:55 AM</p>
+                        </div>
+                        <p className="sidebar__chats-last">Hey how was your day?</p>
+                    </div>
+                </li>
+                
+            </ul>
+        </div>
+    )
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,23 +2,25 @@ import { useDispatch } from 'react-redux'
 import { auth } from '../../firebaseConfig'
 import { update } from 'firebase/database'
 import { signOut } from 'firebase/auth'
+import ChatSidebar from '../components/ChatSidebar'
+import Chat from '../components/Chat'
 
 export default function MainPage() {
     const dispatch = useDispatch()
 
-    async function handleSignout() {
-        try {
-            await signOut(auth)
-            dispatch(update(null))
-        } catch (error) {
-            console.log(error.code)
-        }
-    }
+    // async function handleSignout() {
+    //     try {
+    //         await signOut(auth)
+    //         dispatch(update(null))
+    //     } catch (error) {
+    //         console.log(error.code)
+    //     }
+    // }
 
     return (
-        <>
-            <h1>Main Page</h1>
-            <button onClick={handleSignout}>Sign out</button>
-        </>
+        <main className="main">
+            <ChatSidebar />
+            <Chat />
+        </main>
     )
 }
